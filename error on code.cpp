dff7bad527f7de716290428cd4f4acb07ee4183c
KARNAMSAISANJANA <113425565+KARNAMSAISANJANA@users.noreply.github.com>
2023-03-28 13:10:38 -0400

    Add files via upload

---
+++ error on code.cpp
@@ -0,0 +1,8 @@
+using namespace std;
+#include<io.stream>
+int  main()
+{ int public=10;
+cout<public;
+return 0;
+}
+
---
+++ error.cpp
@@ -0,0 +1,12 @@
+using namespace std;
+#include<iostream>
+int main()
+{
+int i,number;
+for(i=1; i<=6, i++)
+{
+cout<<"Value of variable i is: "<<number<<endl;
+return 0;
+}
+
+
